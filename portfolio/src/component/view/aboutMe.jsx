c1dac58b53d8094f0ae7041149f32cfc7c682e97
Yeahhina <138409780+yeahhina@users.noreply.github.com>
2025-09-20 18:06:40 +0100

    about me page completed

--- portfolio/src/component/view/aboutMe.jsx
+++ portfolio/src/component/view/aboutMe.jsx
@@ -1,29 +1,34 @@
 import "./aboutMe.scss";
+import photo from "./../../data/photo.png";
 function AboutMe() {
   return (
     <div className="aboutMe">
       <h1 id="title">ABOUT ME</h1>
 
       <div className="itemsContainer">
-        <h2 className="subtitle">FULL-STACK & IU DEVELOPER</h2>
-        <hr class="line" />
-        <p>
-          Hi, I’m Yeasmin — a full-stack developer and UI designer based in
-          London. I’m passionate about building responsive, user-friendly
-          websites and applications that combine clean code with innovative
-          design. Skilled in HTML, CSS, JavaScript, and React, I focus on
-          crafting seamless user experiences with visually appealing interfaces.{" "}
-          <br />
-        </p>
-        <p>
-          My coding journey began in sixth form, and since then I’ve worked on
-          diverse projects that strengthened my expertise in both development
-          and design. Beyond coding, I love exploring the latest design trends,
-          reading tech blogs, and deepening my knowledge through my university
-          studies. I’m always eager to learn new technologies and push my skills
-          further to deliver the best possible solutions.
-        </p>
-        <button>Download CV</button>
+        <img src={photo} />
+        <div className="right">
+          <h2 className="subtitle">FULL-STACK & IU DEVELOPER</h2>
+          <hr class="line" />
+
+          <p>
+            Hi, I’m Yeasmin — a full-stack developer and UI designer based in
+            London. I’m passionate about building responsive, user-friendly
+            websites and applications that combine clean code with innovative
+            design. Skilled in HTML, CSS, JavaScript, and React, I focus on
+            crafting seamless user experiences with visually appealing
+            interfaces. <br />
+          </p>
+          <p>
+            My coding journey began in sixth form, and since then I’ve worked on
+            diverse projects that strengthened my expertise in both development
+            and design. Beyond coding, I love exploring the latest design
+            trends, reading tech blogs, and deepening my knowledge through my
+            university studies. I’m always eager to learn new technologies and
+            push my skills further to deliver the best possible solutions.
+          </p>
+          <button>Download CV</button>
+        </div>
       </div>
     </div>
   );
