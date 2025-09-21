577587577321b056d41c15a54bc6b925c2639ea7
Yeahhina <138409780+yeahhina@users.noreply.github.com>
2025-09-21 01:15:54 +0100

    added navigation and removed padding

--- portfolio/src/component/view/header.jsx
+++ portfolio/src/component/view/header.jsx
@@ -1,14 +1,45 @@
 import "./header.scss";
+import { scrollToClass } from "../utility/scrollToElement";
 function Header() {
   return (
     <div className="header">
       <h1 className="title">Yeasmin Mumtahina.</h1>
       <div className="navigationBar">
-        <a >Home</a>
-        <a>Skills</a>
-        <a>Projects</a>
-        <a>About</a>
-        <a>Contact Me</a>
+        <a
+          onClick={() => {
+            scrollToClass("home");
+          }}
+        >
+          Home
+        </a>
+        <a
+          onClick={() => {
+            scrollToClass("skills");
+          }}
+        >
+          Skills
+        </a>
+        <a
+          onClick={() => {
+            scrollToClass("projects");
+          }}
+        >
+          Projects
+        </a>
+        <a
+          onClick={() => {
+            scrollToClass("aboutMe");
+          }}
+        >
+          About
+        </a>
+        <a
+          onClick={() => {
+            scrollToClass("contactMe");
+          }}
+        >
+          Contact Me
+        </a>
       </div>
     </div>
   );
